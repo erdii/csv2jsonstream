58d2513c7fd100dd0782c666f545789e442d29e9
erdii <erdiicodes@gmail.com>
2018-07-06 09:19:44 +0200

    mvp

---
+++ README.md
@@ -0,0 +1,11 @@
+# csv2jsonstream
+
+the cheapest csv to json-array converter EVER!
+
+* uses [papaparse](https://www.papaparse.com/)
+* assumes csv header rows
+* outputs rows in a streaming manner
+
+### usage
+
+`csv2jsonstream input.csv > output.json`
---
+++ csv2jsonstream.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const Papa = require("papaparse");
+
+let state = {
+	inputPath: process.argv[2],
+	notFirstLine: false,
+};
+
+if (!path.isAbsolute(state.inputPath)) {
+	state.inputPath = path.resolve(process.cwd(), state.inputPath);
+}
+
+state.inputFileStream = fs.createReadStream(state.inputPath);
+
+console.log("[");
+
+Papa.parse(state.inputFileStream, {
+	header: true,
+	dynamicTyping: true,
+	step: row => {
+		console.log(
+			(state.notFirstLine ? "," : "") + JSON.stringify(row.data[0])
+		);
+
+		if (!state.notFirstLine) {
+			state.notFirstLine = true;
+		}
+	},
+	complete: () => {
+		console.log("]");
+	}
+});
